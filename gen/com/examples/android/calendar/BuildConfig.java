b7f2c135e2f130e0e20a0055c6d24f3d04d50bf7
Joshua Jamison <joshua.jamison@gmail.com>
2013-05-17 12:45:07 -0600

    removed gen folder

--- gen/com/examples/android/calendar/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.examples.android.calendar;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
